a3eeabd5b43021b469cba66fcfe055baa79dbc06
Hmelium <medbogg@gmail.com>
2019-04-01 18:03:43 +0300

    Final version

---
+++ Archiver/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace Archiver
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ZippingButton_Click(object sender, RoutedEventArgs e)
+        {
+            ZippingWindow zipping = new ZippingWindow();
+            zipping.Show();
+            Close();
+        }
+
+        private void UnzippingButton_Click(object sender, RoutedEventArgs e)
+        {
+            UnzippingWindow unzipping = new UnzippingWindow();
+            unzipping.Show();
+            Close();
+        }
+    }
+}
